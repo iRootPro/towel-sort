80c5db76351e2db847a08c4738732d0b90978fc6
Alexander Neupokoev <admin@armavir.ru>
2020-09-21 10:34:56 +0300

    init: task towel sort

--- src/1.js
+++ src/1.js
@@ -1,7 +1,5 @@
+function towelSort(matrix) {
 
-// You should implement your task here.
-
-module.exports = function towelSort (matrix) {
     if (!Array.isArray(matrix)) return []
     let resultArr = []
     for (let i = 0; i < matrix.length; i++) {
@@ -10,3 +8,9 @@ module.exports = function towelSort (matrix) {
     }
     return resultArr
 }
+
+console.log(towelSort([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+]))
